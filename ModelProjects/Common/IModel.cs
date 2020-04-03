67b74299a0b9634f00b82cbf8675ee7f4261049f
TanaTiX TanaTiX <tanatix@gmail.com>
2020-04-03 22:33:10 +0300

    ...

---
+++ ModelProjects/Common/IModel.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+
+namespace ModelLibrary
+{
+	public interface IModel
+	{
+		event GameOverHandler GameOverHandler;
+		event MoveHandler MoveHandler;
+
+		bool CanMove(CellDto cell);
+		void Move(CellDto cell);
+		int RowsCount { get; }
+		int ColumnsCount { get; }
+	}
+}
--- ModelProjects/ModelLibrary/Model.cs
+++ ModelProjects/ModelLibrary/Model.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ModelLibrary
 {
-    public class Model
+    public class Model:IModel
     {
     }
 }
