fb17bae1b2b0907167dba1f9a04f083dc42eb2c3
TanaTiX TanaTiX <tanatix@gmail.com>
2020-04-03 17:26:51 +0300

    create project

---
+++ ModelProjects/ClassesLibrary/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+	public enum CELL_CONTENT {EMPTY, CROSS, ZERO};
+	public class Cell
+	{
+		private CELL_CONTENT _cell;
+		private int _x;
+		private int _y;
+		public Cell(int x, int y, CELL_CONTENT cell = CELL_CONTENT.EMPTY)
+		{
+			_x = x;
+			_y = y;
+			_cell = cell;
+		}
+		public CELL_CONTENT CellType { get; }//пока только геттер
+		public int X { get; }
+		public int Y { get; }
+	}
+}
---
+++ ModelProjects/CommonClassesLibrary/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassesLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClassesLibrary
+{
+	class Program
+	{
+		public delegate void GameOver(object sender, bool isWin);
+		public delegate void Move(object sender, Cell cell);
+		static void Main(string[] args)
+		{
+			Console.ReadKey();
+		}
+	}
+}
