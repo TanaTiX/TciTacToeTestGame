b5fa803d20ae0c1b2c8449ab7c13d9b7bba805e4
TanaTiX TanaTiX <tanatix@gmail.com>
2020-04-04 00:00:53 +0300

    start debug

--- ModelProjects/DebugConsole/Program.cs
+++ ModelProjects/DebugConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common;
+using ModelLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,10 @@ namespace DebugConsole
 {
     class Program
     {
+        private static IModel model;
         static void Main(string[] args)
         {
+             model = new Model(3, 3, 3);
         }
     }
 }
