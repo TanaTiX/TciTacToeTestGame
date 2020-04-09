a3c8cce518adb1cbe1a9f65c368096c3735d4965
TanaTiX TanaTiX <tanatix@gmail.com>
2020-04-09 15:16:04 +0300

    add App project

--- TicTacToe/AppWPF/App.xaml.cs
+++ TicTacToe/AppWPF/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Common;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using View;
 
 namespace AppWPF
 {
@@ -13,5 +16,28 @@ namespace AppWPF
 	/// </summary>
 	public partial class App : Application
 	{
+		private readonly Window window = new Window();
+		private readonly FirstScreenUC firstScreenUC;
+		private readonly StatisticUC statisticUC;
+		private void OnStartUp(object sender, StartupEventArgs e)
+		{
+			window.Width = 600;
+			window.Height = 700;
+			GetType(typeof(FirstScreenVM));
+		}
+		private void GetType(Type type)
+		{
+			
+
+			switch (type)
+			{
+				case typeof(IFirstScreenVM):
+					window.Content = new FirstScreenVM();
+					break;
+				default:
+					break;
+			}
+			window.Show();
+		}
 	}
 }
