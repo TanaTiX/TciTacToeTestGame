732d7880ae512cf28731cacdc22076542ebeeae6
Хасп Элд <EldHasp@Gmail.com>
2020-04-23 12:02:42 +0300

    Изменил реализацию конвертера в Решении Калькулятора.

--- CalculatorHistory/CalculatorHistory/App.xaml.cs
+++ CalculatorHistory/CalculatorHistory/App.xaml.cs
@@ -19,13 +19,22 @@ namespace CalculatorHistory
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
 			DictionaryKeyToValueConverter conv = (DictionaryKeyToValueConverter)Resources["DictionaryKeyToValueConverter"];
-			conv.SetDictionary(new Dictionary<string, ImageSource>()
-			{
-				{"Addition" , (ImageSource) ImageSourceConverter.ConvertFrom(@"Images\plus.png")},
-				{"Subtraction" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\subt.png")},
-				{"Multiplication" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\multi.png")},
-				{"Division" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\div.png")}
-			});
+			//conv.SetDictionary(new Dictionary<string, ImageSource>()
+			//{
+			//	{"Addition" , (ImageSource) ImageSourceConverter.ConvertFrom(@"Images\plus.png")},
+			//	{"Subtraction" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\subt.png")},
+			//	{"Multiplication" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\multi.png")},
+			//	{"Division" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\div.png")}
+			//});
+
+			/// Можно задать так, а можно и в XAML.
+			/// Но одинаковые ключи будут конфликтовать.
+			/// Поэтому в этой задаче половина ключей здесь,
+			/// а другая в XAML
+			conv.Add("Addition", (ImageSource)ImageSourceConverter.ConvertFrom(@"Images\plus.png"));
+			conv.Add("Subtraction" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\subt.png"));
+			//conv.Add("Multiplication" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\multi.png"));
+			//conv.Add("Division" ,(ImageSource) ImageSourceConverter.ConvertFrom(@"Images\div.png"));
 		}
 	}
 }
