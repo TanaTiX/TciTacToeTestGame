2f7d2d0adbb1aa10f92227c8c13db51ec9883b60
Хасп Элд <EldHasp@Gmail.com>
2020-04-25 14:37:45 +0300

    UserSettingsUC добавлен в тестовый проект. Из него всё убрано кроме TextBlock выводящего Title. По идее получилась копия UserControlTest. Но тем не мене привязка к  UserSettingsUC не работает.

---
+++ CustomUserControl/WpfControlLibrary/UserSettingsUC.xaml.cs
@@ -0,0 +1,15 @@
+﻿using View;
+
+namespace WpfControlLibrary
+{
+	/// <summary>
+	/// Логика взаимодействия для UserSettingsUC.xaml
+	/// </summary>
+	public partial class UserSettingsUC : ListScroll
+	{
+		public UserSettingsUC()
+		{
+			InitializeComponent();
+		}
+	}
+}
