ce07a27e4d04bd4e7a5efb7c9913144171c7ac51
Хасп Элд <EldHasp@Gmail.com>
2020-04-25 11:58:27 +0300

    Добавил в Окна ещё TextBox для вывода Title UC. И два блока с TextBox и кнопкой для изменения свойств VM. Всё работает. Привязки в направлении от VM к UC работаю правильно.

--- CustomUserControl/TestWpfApp/ViewModel.cs
+++ CustomUserControl/TestWpfApp/ViewModel.cs
@@ -3,7 +3,24 @@
 	public class ViewModel : BaseINPC
 	{
 		private string _text;
+		private string _textSecond;
 
 		public string Text { get => _text; set => Set(ref _text, value); }
+		public string TextSecond { get => _textSecond; set => Set(ref _textSecond, value); }
+
+		public RelayCommand ChangeText { get; }
+		public RelayCommand ChangeTextSecond { get; }
+
+		public ViewModel()
+		{
+			ChangeText = new RelayCommand(p =>
+			{
+				Text = p.ToString();
+			});
+			ChangeTextSecond = new RelayCommand(p =>
+			{
+				TextSecond = p.ToString();
+			});
+		}
 	}
 }
